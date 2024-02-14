804f39370970a7084715fd0e4f9e769b68aa1c13
Lunnajera <112588646+Lunnajera@users.noreply.github.com>
2024-02-13 17:30:54 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({ children }: { children: any }) {
               >
               {children}
             </Content>
+            <iframe title="Planner_V1.0 - copia ing" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=e9b572e4-9920-4bdd-acc7-bac08370ae76&autoAuth=true&ctid=3cbcc3d3-094d-4006-9849-0d11d61f484d" frameborder="0" allowFullScreen="true"></iframe>
             <Footer style={{ textAlign: 'center' }}>
               Copyright 2024. Dataset Mexico, All Rights Reserved.
             </Footer>
