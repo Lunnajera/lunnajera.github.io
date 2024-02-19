2b083f02c2da2deaea05b7b1021488a12c03c075
Lunnajera <leonjr.luna@gmail.com>
2024-02-19 14:13:10 -0800

    Updating dbtest

--- pages/dbtest.js
+++ pages/dbtest.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
 import { sql } from "@vercel/postgres";
 
-export default async function Cart({ params }) {
-  // Fetch data within an `async` function
-  const { rows } = await sql`SELECT * from users`;
+export default function Cart({ params }) {
+  const [rows, setRows] = useState(null);
+
+  useEffect(() => {
+    // Create an asynchronous function inside useEffect
+    const fetchData = async () => {
+      try {
+        const result = await sql`SELECT * from users`;
+        setRows(result.rows);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        // Handle errors accordingly
+      }
+    };
+
+    // Call the async function
+    fetchData();
+  }, []); // The empty dependency array ensures useEffect runs only once on mount
 
   // Ensure you have rows before mapping
   if (!rows) return <div>Loading...</div>; // Or a placeholder
+  console.log(rows);
 
   return (
     <div>
-        console
+        Hello world!
     </div>
   );
 }
