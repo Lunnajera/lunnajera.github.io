eb36560d4d78d9b5fe9d5004cb455e1d7e5e6804
Lunnajera <leonjr.luna@gmail.com>
2024-02-19 13:06:21 -0800

    Fixing dbtest

--- pages/dbtest.js
+++ pages/dbtest.js
@@ -1,7 +1,11 @@
 import { sql } from "@vercel/postgres";
 
 export default async function Cart({ params }) {
+  // Fetch data within an `async` function
   const { rows } = await sql`SELECT * from users`;
+
+  // Ensure you have rows before mapping
+  if (!rows) return <div>Loading...</div>; // Or a placeholder
 
   return (
     <div>
