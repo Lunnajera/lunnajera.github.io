f3a087291424573863fe48d0be143e1d21127129
Lunnajera <112588646+Lunnajera@users.noreply.github.com>
2024-02-19 13:08:13 -0800

    Add files via upload

---
+++ pages/dbtest.js
@@ -0,0 +1,19 @@
+import { sql } from "@vercel/postgres";
+
+export default async function Cart({ params }) {
+  // Fetch data within an `async` function
+  const { rows } = await sql`SELECT * from users`;
+
+  // Ensure you have rows before mapping
+  if (!rows) return <div>Loading...</div>; // Or a placeholder
+
+  return (
+    <div>
+      {rows.map((row) => (
+        <div key={row.id}>
+          {row.id} - {row.quantity}
+        </div>
+      ))}
+    </div>
+  );
+}
