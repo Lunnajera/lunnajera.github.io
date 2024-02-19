da1e1fcfa5035e5d7993250b9f7b4de4c4362147
Lunnajera <112588646+Lunnajera@users.noreply.github.com>
2024-02-19 07:15:24 -0800

    Update Accordion.js

--- components/Accordion/Accordion.js
+++ components/Accordion/Accordion.js
@@ -11,9 +11,9 @@ const accordionData = [
         isOpen: true,
         content:
         <pre>
-            This is line 1
-            This is line 2
-            This is line 3
+            This is line 1<br />
+            This is line 2<br />
+            This is line 3<br />
         </pre>
     },
     {
