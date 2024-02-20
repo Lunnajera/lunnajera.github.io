8583372bdab427ea96ac430e02943b3054505cc6
Lunnajera <leonjr.luna@gmail.com>
2024-02-19 16:21:22 -0800

    Try 998289349823498234

--- next.config.js
+++ next.config.js
@@ -22,7 +22,16 @@ const nextConfig = {
         siteKeywords: "your company keywords",
         siteUrl: "You site url",
         siteImagePreviewUrl: "/images/preview.jpeg",
-        twitterHandle: "@your_handle"
+        twitterHandle: "@your_handle",
+        POSTGRES_URL: "postgres://default:hO0MGlwga9kX@ep-jolly-violet-a6oz5sk9-pooler.us-west-2.aws.neon.tech/verceldb?sslmode=require",
+        POSTGRES_URL_NON_POOLING: "postgres://default:hO0MGlwga9kX@ep-jolly-violet-a6oz5sk9.us-west-2.aws.neon.tech/verceldb?sslmode=require",
+        POSTGRES_URL_NO_SSL: "postgres://default:hO0MGlwga9kX@ep-jolly-violet-a6oz5sk9-pooler.us-west-2.aws.neon.tech/verceldb",
+        POSTGRES_PRISMA_URL: "postgres://default:hO0MGlwga9kX@ep-jolly-violet-a6oz5sk9-pooler.us-west-2.aws.neon.tech/verceldb?pgbouncer=true&connect_timeout=15&sslmode=require",
+        POSTGRES_USER: "default",
+        POSTGRES_PASSWORD: "hO0MGlwga9kX",
+        POSTGRES_HOST: "ep-jolly-violet-a6oz5sk9-pooler.us-west-2.aws.neon.tech",
+        POSTGRES_DATABASE: "verceldb"
+
     },
     headers() {
         return [
