326f2e247c61e2598c3cdce94790752efa174083
Lunnajera <cr1050204@bose.com>
2024-02-27 11:25:29 -0800

    Multiple changes

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 
 const navigation = [
     { name: "Caracteristicas", to: "#features", href: "/#features" },
-    { name: "Testimonios", to: "#testimonials", href: "/#testimonials" },
+    { name: "Cultura", to: "#testimonials", href: "/#testimonials" },
     { name: "FAQ?", to: "#faq", href: "/#faq" },
     {
         name: "Contacto",
