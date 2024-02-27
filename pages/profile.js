8d88e640e01a27ab92968303fbd34496face867f
Lunnajera <cr1050204@bose.com>
2024-02-27 09:26:56 -0800

    adding bi

--- pages/profile.js
+++ pages/profile.js
@@ -10,7 +10,7 @@ console.log("Hello");
     <div className='layout'>
     <Header />
     <div className="flex flex-col items-center md:flex-row md:h-screen">
-      Place power bi here
+    <iframe title="Planner_V1.0 - copia ing" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=e9b572e4-9920-4bdd-acc7-bac08370ae76&autoAuth=true&ctid=3cbcc3d3-094d-4006-9849-0d11d61f484d" frameborder="0" allowFullScreen></iframe>
     </div>
   </div>
   );
