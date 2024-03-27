6e2fd30c4ecfaa00332f9cb180ad6a6ee633e1dd
Carlos-Rodriguez-Bose <cr1050204@bose.com>
2024-03-27 16:03:30 -0700

    change image

--- components/Columns_clients/Columns_clients.js
+++ components/Columns_clients/Columns_clients.js
@@ -10,7 +10,7 @@ const ColumnData = [
         content:
             "",
         align: "center",
-        image: "/valle2.png",
+        image: "/valle3.png",
         width: "800",
         height: "190"
     }
